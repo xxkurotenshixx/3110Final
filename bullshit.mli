462fae159419c595dc5929661efe4470d486adeb
nicole-tan <nat34@cornell.edu>
2015-11-05 16:32:24 -0500

    Updated the beginning types for bullshit.ml

--- bullshit.mli
+++ bullshit.mli
@@ -1,19 +1,29 @@
-(**
-  * [run bet] simulates a round of Bullshit.
-  *
-  * It takes a bet amount and returns true if the game is won.
-  *)
-val run : int -> bool
 
 (**
   * [parse] runs the game
   *
   *)
-
 val parse : unit -> bool
+
+(**
+  * [game_start]
+  *
+  *)
+
+val game_start : unit ->
+
+(**
+  * [run bet] simulates a round of Bullshit.
+  *
+  * The player begins with a betted amount. It returns true if the game is won.
+  *)
+val run : int -> bool
+
 
 (**
   *
   *
   *)
-val
+val quit :
+
+(*false means lose*)
