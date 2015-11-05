1f3774bf54382e5eed80f7b421ae2c03af44e715
nicole-tan <nat34@cornell.edu>
2015-11-05 15:52:26 -0500

    Began the mli file

---
+++ bullshit.mli
@@ -0,0 +1,19 @@
+(**
+  * [run bet] simulates a round of Bullshit.
+  *
+  * It takes a bet amount and returns true if the game is won.
+  *)
+val run : int -> bool
+
+(**
+  * [parse] runs the game
+  *
+  *)
+
+val parse : unit -> bool
+
+(**
+  *
+  *
+  *)
+val
