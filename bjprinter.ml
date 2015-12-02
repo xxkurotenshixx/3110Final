8772cabf6355d657e0948a7fe3dac041fa038671
nicole-tan <nat34@cornell.edu>
2015-12-02 16:15:53 -0500

    Added rules for blackjack

--- bjprinter.ml
+++ bjprinter.ml
@@ -13,12 +13,27 @@ let game_won () =
 let quit () =   Printf.printf "Are you sure you wish to leave? \
                You will lose your bet. (y/n)\n"
 
-let welcome () = "unimplemented"
+let welcome () = Printf.printf "WELCOME TO A GAME OF BLACKJACK!"
 
 
 let help () = "unimplemented"
 
-let rules () = "unimplemented"
+let rules () = Printf.printf "So you want to play a game of Blackjack? \
+In Blackjack, the rules are simple: get 21 or get closer to 21 than the
+dealer. The dealer is your only opponent.
+
+The game starts with you, the player, receiving two cards. The dealer also
+receives two cards; however, you can only see the first.
+
+Each card has a value, with the numbered cards having the value given, the
+face cards (King, Queen, and Jack) having values of 10 a piece, and finally
+the Ace. The Ace can either have a value of 1 or 11.
+
+The player can choose to type 'hit' or 'stand' after seeing the given cards.
+To 'hit' means to add another card to your hand. To 'stand' means to finish
+your turn. Once you type 'stand', it becomes the dealer's turn. When the
+dealer finishes, cards are compared and the number closest to 21 wins!
+"
 
 let print_hand cards =
   let rec helper = function
