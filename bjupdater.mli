a9d1e238cb3b6b462718c12fc3ed2c979ce5f70b
nicole-tan <nat34@cornell.edu>
2015-12-03 15:19:35 -0500

    updated mli file

--- bjupdater.mli
+++ bjupdater.mli
@@ -1,11 +1,14 @@
 module Bjupdater:
 sig
 
-  
-  val sum: Card.card list -> (bool *  int)
 
-  (*is p a string in hit s p?*)
-  val hit: state -> Card.card list -> state
+  val sum: Card.card list -> int
+
+  val winner: state -> state
+
+  val hit: state -> string -> state
+
+  val round_won: int -> state -> state
 
   val new_game_deal: int -> state -> Card.card list
 
@@ -17,6 +20,14 @@ sig
 
   val player: state -> state
 
-  val finished: state -> state 
+  val finished: state -> state
+
+  val pbust: state -> bool
+
+  val dbust: state -> bool
+
+  val ddraw: state -> bool
+
+  val winner: state -> int
 
 end
