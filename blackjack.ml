844c776d22d80beedd3436b41657abd6c9b08955
xxkurotenshixx <babycarr6193@gmail.com>
2015-12-02 22:49:33 -0500

    changes to bj data structure;

--- blackjack.ml
+++ blackjack.ml
@@ -6,20 +6,22 @@ module Blackjack = struct
     then
 
   let run_round bid os =
-    let s = Bjupdater.new_game_deal os in
-    let cl = Hbj.
+    let s = Bjupdater.new_game_deal bid os in
+    let cl = player_turn os
 
-  let rec runner money =
-    Bjprinter.rules ();
+  let runner s =
     Bjprinter.start_prompt ();
-    let (start,bid) = Hbj.start () in
-    if start
+    let bid = Hbj.start Bjupdater.money s in
+    if bid > 0
     then
-
+      run_round bid s
+    else
+      s
 
   let run money =
     Bjprinter.welcome ();
-    let (money_left,rounds) = runner money in
-    Bjprinter.leave money money_left rounds;
-    money_left
+    Bjprinter.rules ();
+    let s = runner (Bjupdater.new_table money) in
+    Bjprinter.leave money (Bjupdater.money s) (Bjupdater.rounds s);
+    Bjupdater.money s
 end
