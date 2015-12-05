7be615db0bfe47aa8c48bf2026a441eaf430c701
nicole-tan <nat34@cornell.edu>
2015-12-05 15:59:09 -0500

    Update parser.mli

--- bs/parser.mli
+++ bs/parser.mli
@@ -1,9 +1,9 @@
-(* [parse ()] reads the user input and returns a list of lowercase strings
+(* [parse] reads the user input and returns a list of lowercase strings
  *  split according to the spaces found in the input. Returned list is
  *  guaranteed to have a length greater than 0 and strings of length 1
  *  or greater. *)
 val parse : unit -> string list
 
-(* [confirm ()] reads the user input and returns true if the user confirms
+(* [confirm] reads the user input and returns true if the user confirms
  * their coice and false otherwise. *)
 val confirm: unit -> bool
