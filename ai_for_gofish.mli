7df6b8090152fe8ee06ca6dbff79a0fcb60a430c
robaga17 <rra46@cornell.edu>
2015-12-05 19:12:48 -0500

    done

--- ai_for_gofish.mli
+++ ai_for_gofish.mli
@@ -1,8 +1,10 @@
 (**
- * [ai_turn ai gs] will decide what action [ai] will perform. gs
- * will be mutated based on what action is chosen. If that turn ends the
- * game, returns unit. If the game is still not over, it will call
- * the next player's turn.
- * (The game will eventually end, untimately returning unit.)
+ * [choose_random hand] will return a random card in hand
  *)
-val ai_turn : player -> game_state -> unit
+val choose_random : Card.card list -> Card.rank
+
+
+(**
+ * [ai_decide hand choices] will return the most reasonable rank to ask for
+ *)
+val ai_decide : Card.card list -> Card.rank list -> Card.rank option
